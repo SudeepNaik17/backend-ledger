25caa147fa8e2011a89b1cb874c90fa3f331a752
Sudeep <sudeepnaik801@gmail.com>
2026-06-09 00:11:20 +0530

    updated the auth middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -24,7 +24,32 @@ async function authMiddleware(req, res, next) {
     });
   }
 }
+async function authSystemUserMiddleware(req, res, next) {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    res.status(401).json({
+      message: "Unauthorised access, token is missing",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded.userId).select("+systemUser");
+    if (!user.systemUser) {
+      return res.status(403).json({
+        message: "Forbidden acsess,not a system user",
+      });
+    }
+    req.user = user;
+    return next();
+  } catch (e) {
+    return res.status(401).json({
+      message: "Unauthorized access,token is invalid",
+    });
+  }
+}
 
 module.exports = {
   authMiddleware,
+  authSystemUserMiddleware,
 };
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,7 +1,25 @@
 const { authMiddleware } = require("../middlewares/auth.middleware");
-const { createTransaction } = require("../controllers/transaction.controller");
+const {
+  createTransaction,
+  createInitialFundsTransaction,
+} = require("../controllers/transaction.controller");
 const express = require("express");
+const { authSystemUserMiddleware } = require("../middleware/auth.middleware");
 const router = express.Router();
 
+/**
+ * - POST /api/transactions/
+ * - Create a new transaction
+ */
 router.post("/", authMiddleware, createTransaction);
+
+/**
+ * - POST /api/transactions/system/initial-funds
+ * - Create initial funds transaction from system user
+ */
+router.post(
+  "/system/initial-funds",
+  authSystemUserMiddleware,
+  createInitialFundsTransaction,
+);
 module.exports = router;
